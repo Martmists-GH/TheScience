25c1dd794c93be0d7e38835d8d5ff4ab05b2b580
martmists <martmists@gmail.com>
2017-11-24 17:11:52 +0100

    start work on Level

--- cogs/level.py
+++ cogs/level.py
@@ -1,4 +1,10 @@
 import math
 
-def get_level():
-    pass
+def get_xp_needed(level):
+    return level * math.log(level)
+
+
+class Level:
+    def __init__(self, bot):
+        role = bot.config.scientist_role
+        data = bot.data
